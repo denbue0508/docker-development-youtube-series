aed2e337dd652db0814e348ee851355e6cb911ce
Jayson Catacutan <jaysoncatacutan@Drogon.local>
2022-12-13 15:11:00 +0800

    - Add payment modules`

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import matrix from './v1/matrix';
+import payment from './v1/payment';
 import device from './v1/device';
 import verifyToken from '../helpers/verifyToken';
 
@@ -8,5 +8,5 @@ export const v1: Router = Router();
 /* MIDDLEWARE FOR AUTHENTICATION */
 v1.use(verifyToken);
 
-v1.use('/heatmap', matrix);
+v1.use('/payment', payment);
 v1.use('/device', device);
