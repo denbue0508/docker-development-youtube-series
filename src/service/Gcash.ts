b388eb580f367041af20a52d68289a1eb74c170c
Marcelo S Cantong II <iamcelo@Marcelos-MacBook-Pro.local>
2023-01-25 09:17:28 +0800

    CHORE - optimize code by params

--- src/service/Gcash.ts
+++ src/service/Gcash.ts
@@ -4,19 +4,16 @@ dotenv.config();
 
 import axios from "axios";
 class Gcash {
-  private CLIENT_ID: string;
   private PATH: string;
   private URL: string;
 
   constructor(path: string) {
-    this.CLIENT_ID = process.env.REFERENCE_CLIENT_ID
     this.PATH = path;
     this.URL = `${process.env.GCASH_BASE_URL}${this.PATH}`
   }
 
   public post = async (payload: any) => {
     const headers = setHeader(
-      this.CLIENT_ID,
       `${this.PATH}`,
       payload
     );
