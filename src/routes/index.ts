86d2a21b8c384d994170d0a9ae9bd4ce99507225
Marcelo S Cantong II <iamcelo@Marcelos-MacBook-Pro.local>
2023-01-31 16:21:43 +0800

    CHORE - remove unused import

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import authorization from './v1/authorization';
 import payment from './v1/payment';
 import refund from './v1/refund';
 import transaction from './v1/transaction';
-// import verifyToken from '../helpers/verifyToken';
 
 export const v1: Router = Router();
 
